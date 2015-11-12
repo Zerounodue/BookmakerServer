520854b6bfe07e2ec2b03424ab2f7b4c4b674a98
luthp3 <luthp3@bfh.ch>
2015-11-12 15:27:30 +0100

    added some stuff

---
+++ AWTBookmakerServer/src/java/user/UserBean.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package user;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import model.User;
+
+/**
+ *
+ * @author bizki
+ */
+@ManagedBean
+@SessionScoped
+public class UserBean {
+    private User user;
+    private String locale;
+    
+    
+    
+    
+    
+    
+    
+    public void setLocale(String locale){
+	this.locale = locale;
+    }
+    
+    public String getLocale(){
+	if(locale == null){
+            FacesContext context = FacesContext.getCurrentInstance();
+            return  context.getViewRoot().getLocale().toString();
+	}
+	return locale;
+    }
+    
+    
+}
