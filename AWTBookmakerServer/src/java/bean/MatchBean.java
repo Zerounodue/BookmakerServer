8d9f11f13f744e80e79766f4b652a77b78cd1883
luthp3 <luthp3@bfh.ch>
2015-11-16 22:35:11 +0100

    added login and register, without db layout improved establishment of db connection improved used sql statements

--- AWTBookmakerServer/src/java/bean/MatchBean.java
+++ AWTBookmakerServer/src/java/bean/MatchBean.java
@@ -6,7 +6,6 @@
 package bean;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -17,7 +16,10 @@ import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import model.Match;
 import java.sql.Timestamp;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import model.Team;
+import util.DBHelper;
 
 /**
  *
@@ -27,59 +29,52 @@ import model.Team;
 @SessionScoped
 public class MatchBean {
 
+    private final static String SELECT_ALL_FROM_MATCHES
+            = "SELECT m.id, m.homeTeamFK, m.awayTeamFK, m.time FROM matches m "
+            + "INNER JOIN teams ht ON m.homeTeamFK = ht.id "
+            + "INNER JOIN teams at ON m.awayTeamFK = at.id ";
+
+    /**
+     * Gets all matches with a start time > than now
+     * @return list of Match objects or null
+     */
     public List<Match> getUpcomingMatches() {
         List<Match> matches = null;
+        //get connection to DB
+        Connection conn = DBHelper.getDBConnection();
+        //error connecting to db
+        if (conn != null) {
+            ResultSet rs = null;
+            PreparedStatement s = null;
 
-        //put some place that is more useful
-        Connection conn = null;
-        try {
-            String userName = "bookmaker";
-            String password = "b00kM4k3r";
-            String url = "jdbc:mysql://localhost/bookmaker";
-            Class.forName("com.mysql.jdbc.Driver").newInstance();
-            conn = DriverManager.getConnection(url, userName, password);
-            System.out.println("Database connection established");
-        } catch (Exception e) {
-            System.err.println("Cannot connect to database server" + e.toString());
-        }
-        //TODO find better way
-        if(conn == null) return null;
-        
-        try {
+            try {
+                Timestamp currentTimestamp = new Timestamp(Calendar.getInstance().getTime().getTime());
 
-            java.sql.Timestamp currentTimestamp = new java.sql.Timestamp(Calendar.getInstance().getTime().getTime());
+                String sql = SELECT_ALL_FROM_MATCHES
+                        + "WHERE m.time > '" + currentTimestamp.toString() + "'"
+                        + "ORDER BY m.time asc ";
 
-            PreparedStatement s;
+                s = conn.prepareStatement(sql);
+                rs = s.executeQuery();
+                if (rs != null) {
+                    matches = new ArrayList<>();
 
-            String sql = "SELECT m.id, m.homeTeamFK, m.awayTeamFK, m.time FROM matches m "
-                    + "INNER JOIN teams ht ON m.homeTeamFK = ht.id "
-                    + "INNER JOIN teams at ON m.awayTeamFK = at.id "
-                    + "WHERE m.time > '" + currentTimestamp.toString() + "'"
-                    + "ORDER BY m.time asc ";
-
-            //String sql = "INSERT INTO `user` (`userID` ,`username` ,`password` )VALUES (NULL , ?, ?)";
-            s = conn.prepareStatement(sql);
-            ResultSet rs = s.executeQuery();
-            if (rs != null) {
-                matches = new ArrayList<>();
-
-                while (rs.next()) {
-                    int id = rs.getInt("id");
-                    int htId = rs.getInt("homeTeamFK");
-                    int atId = rs.getInt("awayTeamFK");
-                    Timestamp ts = rs.getTimestamp("time");
-                    //result will be null by upcoming matches
-                    Match m = new Match(id, ts, new Team(htId), new Team(atId), null);
-
-                    matches.add(m);
+                    while (rs.next()) {
+                        int id = rs.getInt("id");
+                        int htId = rs.getInt("homeTeamFK");
+                        int atId = rs.getInt("awayTeamFK");
+                        Timestamp ts = rs.getTimestamp("time");
+                        //result will be null by upcoming matches
+                        Match m = new Match(id, ts, new Team(htId), new Team(atId), null);
+                        matches.add(m);
+                    }
                 }
-                rs.close();
+
+            } catch (SQLException e) {
+                Logger.getLogger(MatchBean.class.getName()).log(Level.SEVERE, null, e);
+            } finally {
+                DBHelper.closeConnection(rs, s, conn);
             }
-
-            s.close();
-
-        } catch (SQLException e) {
-            System.out.println(e);
         }
 
         return matches;
---
+++ AWTBookmakerServer/src/java/util/DBHelper.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+/**
+ *
+ * @author bizki
+ */
+public class DBHelper {
+
+    private static final String URL = "jdbc:mysql://localhost/bookmaker";
+    private static final String DRIVER_PATH = "com.mysql.jdbc.Driver";
+    //TODO make more secure
+    private static final String USER_NAME = "bookmaker";
+    private static final String PASSWORD = "b00kM4k3r";
+    
+    /**
+     * Establishes a connection to a sql DB
+     * @return A sql DB connection or null
+     */
+    public static Connection getDBConnection(){
+        Connection conn = null;
+        try {
+            Class.forName(DRIVER_PATH).newInstance();
+            conn = DriverManager.getConnection(URL, USER_NAME, PASSWORD);
+        } catch (Exception e) {
+            System.err.println(e.toString());
+        }
+        return conn;
+    }
+    
+    /**
+     * Tries to close the db connection
+     * @param r Result set to close
+     * @param s Statements to close
+     * @param c Connection to close
+     */
+    public static void closeConnection(ResultSet r, PreparedStatement s, Connection c){
+        try { if (r != null) r.close(); } catch (Exception e) {}
+        try { if (s != null) s.close(); } catch (Exception e) {}
+        try { if (c != null) c.close(); } catch (Exception e) {}
+    }
+    
+    
+}
