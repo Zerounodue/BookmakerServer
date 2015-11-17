55a027f9501c5877091a86d51924a86afcf0e2ea
luthp3 <luthp3@bfh.ch>
2015-11-17 17:44:55 +0100

    added a bit for register and login

--- AWTBookmakerServer/src/java/bean/LoginBean.java
+++ AWTBookmakerServer/src/java/bean/LoginBean.java
@@ -19,7 +19,11 @@ import model.User;
 public class LoginBean {
     private User user;
     private String locale;
+    private String userName;
+    private String password;
     
+    private final static String LOGIN_SITE = "login.xhtml?faces-redirect=true";
+    private final static String HOME_SITE = "../home.xhtml?faces-redirect=true";
 
     public void setLocale(String locale){
 	this.locale = locale;
@@ -32,6 +36,53 @@ public class LoginBean {
 	}
 	return locale;
     }
+    
+    
+    public String login(){
+        
+        
+        int a = 3;
+        
+        
+        return HOME_SITE;	
+    }
+
+    public String register(){
+        
+        
+        
+        return LOGIN_SITE;
+    }
+    
+    /**
+     * @return the userName
+     */
+    public String getUserName() {
+        return userName;
+    }
+
+    /**
+     * @param userName the userName to set
+     */
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    /**
+     * @return the password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * @param password the password to set
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    
     
     
 }
